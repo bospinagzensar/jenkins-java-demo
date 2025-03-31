0e9e2b3da311c9faa58db848ad92fe927de71b91
bospinagzensar <brian.ospinagonzalez@zensar.com>
2025-03-31 11:27:55 -0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,6 @@
-public class Hello{
-	public static void main(String args[]){
-		System.out.println(“Hello ”)
-		System.out.println(“Thank you from Jenkins”)
-
-  }
-} 
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+        System.out.println("Thank you from Jenkins");
+    }
+}
